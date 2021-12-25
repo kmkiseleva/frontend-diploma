f4d1aa81cdf184c929fc4cb22ac339b997354cf5
Ksenia Kiseleva <ksushakiseleva@mail.ru>
2021-12-25 23:36:56 +0300

    stream

--- src/components/Pickers/DestinationPicker/DestinationPickerInput.js
+++ src/components/Pickers/DestinationPicker/DestinationPickerInput.js
@@ -2,64 +2,95 @@ import "./destinationPicker.css";
 import { AutoComplete, Input } from "antd";
 import { LoadingOutlined } from "@ant-design/icons";
 import React, { memo, useMemo, useState } from "react";
-import { BehaviorSubject, Observable, of } from "rxjs";
-// import {
-//   autocomplete,
-//   fetch$,
-//   ResponseOptions,
-// } from "../../../utils/throttling";
+import { BehaviorSubject, of } from "rxjs";
 import { ReactComponent as GeoIcon } from "../../../img/geoIcon.svg";
+import { autocomplete, fetch$ } from "../../../utils/streaming";
 
-export default function DestinationPickerInput({
-  className,
-  point,
-  defaultValue,
-  onSelect,
-}) {
-  const [loading, setLoading] = useState(false);
-  const [options, setOptions] = useState([]);
-  const [value, setValue] = useState(defaultValue);
+const DestinationPickerInput = memo(
+  ({ className, point, defaultValue, onSelect }) => {
+    const [loading, setLoading] = useState(false);
+    const [options, setOptions] = useState([]);
+    const [value, setValue] = useState(defaultValue);
 
-  // const term$ = useMemo(
-  //   () => new BehaviorSubject(defaultValue),
-  //   [defaultValue]
-  // );
-  // const results$ = useMemo(
-  //   () => term$.pipe(autocomplete(1000, (term): Observable => fetch$(term))),
-  //   [term$]
-  // );
+    // стрим (rxjs) с оптимизацией при рендеринге (useMemo)
+    const stream$ = useMemo(
+      () => new BehaviorSubject(defaultValue),
+      [defaultValue]
+    );
 
-  // Обновление значения инпута
-  React.useEffect(() => {
-    setValue(defaultValue);
-  }, [defaultValue]);
+    const cities$ = useMemo(
+      () => stream$.pipe(autocomplete(1000, (val) => fetch$(val))),
+      [stream$]
+    );
 
-  return (
-    <AutoComplete
-      className={className}
-      notFoundContent="введите название города..."
-      backfill
-      dropdownMatchSelectWidth
-      defaultOpen={false}
-      value={value}
-      options={options}
-      filterOption={(inputValue, option) =>
-        option?.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
+    // подписка на загрузку состояния
+    React.useEffect(() => {
+      const loadingSub = stream$.subscribe({
+        next: (val) => {
+          if (val !== defaultValue) {
+            setLoading(true);
+          }
+          setValue(val);
+        },
+      });
+      return () => loadingSub.unsubscribe();
+    }, [stream$, defaultValue]);
+
+    // Обновление значения инпута
+    React.useEffect(() => {
+      setValue(defaultValue);
+    }, [defaultValue]);
+
+    const onChangeInput = (payload) => {
+      stream$.next(payload);
+    };
+
+    // подписка на стрим инпута
+    React.useEffect(() => {
+      const sub = cities$.subscribe({
+        next: (val) => {
+          setLoading(false);
+          setOptions(val);
+        },
+        error: (e) => of(e),
+      });
+      return () => sub.unsubscribe();
+    }, [cities$]);
+
+    const returnSelectedCity = (payload, arrayOfCities, point) => {
+      const selectedCity = arrayOfCities.find((el) => el.value === payload);
+      if (selectedCity) {
+        onSelect(selectedCity, point);
       }
-      // onSelect={(payload) => returnSelectedCity(payload, options, point)}
-      // onChange={(payload) => {
-      //   term$.next(payload);
-      // }}
-    >
-      <Input
-        className="destination__autocomplete"
-        placeholder={point === "departure" ? "Откуда" : "Куда"}
-        suffix={
-          <div className="destination__geoIcon">
-            {loading ? <LoadingOutlined /> : <GeoIcon />}
-          </div>
+    };
+
+    return (
+      <AutoComplete
+        className={className}
+        notFoundContent="введите название города..."
+        backfill
+        dropdownMatchSelectWidth
+        defaultOpen={false}
+        value={value}
+        options={options}
+        filterOption={(inputValue, option) =>
+          option?.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
         }
-      />
-    </AutoComplete>
-  );
-}
+        onChange={onChangeInput}
+        onSelect={(payload) => returnSelectedCity(payload, options, point)}
+      >
+        <Input
+          className="destination__autocomplete"
+          placeholder={point === "departure" ? "Откуда" : "Куда"}
+          suffix={
+            <div className="destination__geoIcon">
+              {loading ? <LoadingOutlined /> : <GeoIcon />}
+            </div>
+          }
+        />
+      </AutoComplete>
+    );
+  }
+);
+
+export default DestinationPickerInput;
